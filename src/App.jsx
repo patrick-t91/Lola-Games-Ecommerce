76eabd954eeb4b9dd4829a58fc092f01b7158634
Patricio Toribio <toribio.patricio@gmail.com>
2021-06-30 18:38:08 -0300

    desafio 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import { NavBar } from './Components/NavBar/Index.jsx'
+import { NavBar } from './Components/NavBar'
 import { ItemListContainer } from './Containers/ItemListContainer/ItemListContainer.jsx'
 
 const App = () => {
